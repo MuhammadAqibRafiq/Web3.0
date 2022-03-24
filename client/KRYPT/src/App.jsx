c20e4b5b240a38af909ca647072da2f306275c13
Muhammad Aqib Rafiq <aqibrafiq001@outlook.com>
2022-03-24 20:40:48 +0500

    first commit

---
+++ client/KRYPT/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {Transactions,Services,Navbar,Footer,Loader} from "./component/index"
+
+const App = () => {
+
+  return (
+    <div className="App">
+     as
+    </div>
+  )
+}
+
+export default App
