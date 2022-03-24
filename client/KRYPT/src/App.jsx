3b7f867132671e222ed1b460fd3579b4d578cc58
Muhammad Aqib Rafiq <aqibrafiq001@outlook.com>
2022-03-24 22:16:27 +0500

    add some packages

--- client/KRYPT/src/App.jsx
+++ client/KRYPT/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
-import {Transactions,Services,Navbar,Footer,Loader} from "./component/index"
+import { Transactions, Services, Navbar, Footer, Loader, Welcome } from "./component/index"
 
 const App = () => {
 
   return (
-    <div className="App">
-     as
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Transactions />
+      <Services />
+      <Footer />
+      <Loader />
     </div>
   )
 }
--- client/KRYPT/src/component/Navbar/Navbar.jsx
+++ client/KRYPT/src/component/Navbar/Navbar.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiMenuAlt4 } from "react-icons/hi"
+import { AiOutlineClose } from "react-icons/ai"
 
+import logo from "../../../images/logo.png"
+
+const NavbarItems = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>
+    {title}
+  </li>
+}
 const Navbar = () => {
+
+  const [toggleMenu, settoggleMenu] = useState(false)
+
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+
+      <div className='md:flex:[0.5] flex-initial justify-center items-center'>
+        <img src={logo} alt="logo" className='w-32 cursor-pointer' />
+      </div>
+
+      <ul className='text-white md:flex hidden list-none flex-row flex-initial justify-center items-center'>
+
+        {["Market", "Exchange", "Tutorials", "Wallets"].map((items, key) => (
+          <NavbarItems title={items} key={key} />
+        ))}
+
+        <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+          Login
+        </li>
+      </ul>
+
+      {!toggleMenu ?
+        <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => settoggleMenu(true)} />
+        :
+        <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => settoggleMenu(false)} />
+      }
+      {
+        toggleMenu && (
+          <ul>
+            <li className='text-xl w-full my-2'>
+              <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => settoggleMenu(false)} />
+            </li>
+            {["Market", "Exchange", "Tutorials", "Wallets"].map((items, key) => (
+              <NavbarItems title={items} key={key} classProps="my-2 text-lg" />
+            ))}
+          </ul>
+        )
+      }
+    </nav>
   )
 }
 
